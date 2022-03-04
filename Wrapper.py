7dbbce04250d2c689b016947e8110e05cdb996ae
Bo-Shiang <bert41003@gmail.com>
2022-03-04 11:48:43 -0500

    write function for thin plate spline warping

--- Wrapper.py
+++ Wrapper.py
@@ -94,8 +94,63 @@ def triangulation_warping(src, src_tri, dst_tri):
     dst = scipy.interpolate.interp2d(src_x, src_y, src_z, kind='cubic')
     return dst
 
-def traditional(src, dst, src_pts, dst_pts):
+def U(r):
+    res = (r**2) * np.log(r**2)
+    return res
 
+def tps_model(src_1d, dst_pts):
+    p = len(dst_pts)
+    K_mat = np.zeros((p, p), np.float32) # size: p x p
+    P_mat = np.zeros((p, 3), np.float32) # size: p x 3
+    for i in range(p):
+        for j in range(p):
+            K_mat[i, j] = U(np.linalg.norm(dst_pts[i, i] - dst_pts[j, j]))
+    P_mat = np.hstack((P_mat, np.ones((dst_pts, 1))))
+    
+    K_P_Pt_mat_row1 = np.hstack((K_mat, P_mat))
+    K_P_Pt_mat_row2 = np.hstack((P_mat.T, np.zeros((P_mat.T.shape[0], P_mat.T.shape[1]))))
+    K_P_Pt_mat = np.vstack((K_P_Pt_mat_row1, K_P_Pt_mat_row2))
+    lamda = 0.0001
+    A_mat = K_P_Pt_mat + lamda * np.identity((p + 3))
+    v_o = np.concatenate((src_1d, [0, 0, 0]))
+    est_params = np.linalg.inv(A_mat) @ v_o
+    return est_params
+
+def f(x_param, y_param, p, dst_pts, img_warp_row, img_warp_col):
+    ax_x, ay_x, a1_x = x_param[p], x_param[p+1], x_param[p+2]
+    ax_y, ay_y, a1_y = y_param[p], y_param[p+1], y_param[p+2]
+    f1, f2 = 0, 0
+    for i in range(p):
+        f1 += x_param[i] * U(np.linalg.norm(dst_pts[i] - img_warp_row))
+        f2 += y_param[i] * U(np.linalg.norm(dst_pts[i] - img_warp_col))
+    f1 += a1_x + ax_x * img_warp_row + ay_x * img_warp_col
+    f2 += a1_y + ax_y * img_warp_row + ay_y * img_warp_col
+    return f1, f2
+
+def thin_plate_spline_warping(src, dst, src_pts, dst_pts):
+    src_pts = np.array(src_pts)
+    dst_pts = np.array(dst_pts)
+    p = len(src_pts)
+    r = cv2.boundingRect(np.float32([dst_pts]))
+    mask = np.zeros((r[3], r[2], 3), np.float32)
+    warped_img = np.copy(mask)
+
+    est_params_x = tps_model(src[:, 0], dst_pts)
+    est_params_y = tps_model(src[:, 1], dst_pts)
+    for i in range(warped_img.shape[1]):
+        for j in range(warped_img.shape[0]):
+            f1, f2 = f(est_params_x, est_params_y, p, dst_pts, i + r[0], j + r[1])
+            warped_img[j, i] = src[f2, f1, :] 
+    warped_img *= mask
+    dst[r[1]:r[1]+r[3], r[0]:r[0]+r[2]] = dst[r[1]:r[1]+r[3], r[0]:r[0]+r[2]] * ( (1.0, 1.0, 1.0) - mask )
+    dst[r[1]:r[1]+r[3], r[0]:r[0]+r[2]] = dst[r[1]:r[1]+r[3], r[0]:r[0]+r[2]] + warped_img
+    return dst
+
+def traditional(src, dst, src_pts, dst_pts, method):
+    if method == 'tri':
+        res = triangulation_warping(src, src_pts, dst_pts)
+    elif method == 'tps':
+        res = thin_plate_spline_warping(src, dst, src_pts, dst_pts)
     return res
 
 def main():
